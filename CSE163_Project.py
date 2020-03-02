dc1fc3200df3befcde8368b25ee1bd3493062b48
libiyao <50092961+libiyao@users.noreply.github.com>
2020-03-02 15:41:41 -0800

    m

--- CSE163_Project.py
+++ CSE163_Project.py
@@ -1,8 +1,7 @@
 import pandas as pd
 
-<<<<<<< HEAD
 def question1():
-
+    
 
 def read_files(file):
     return pd.read_csv(file)
@@ -10,9 +9,6 @@ def read_files(file):
 
 def main():
     question1()
-=======
-def main():
->>>>>>> a7bae8226eead86810debf252d58739ab4ae9fd5
 
 
 if __name__ == '__main__':
