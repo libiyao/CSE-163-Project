8560ea54f13cd8cb5cfd37acc9a9fdf25067a876
Alyssa Allums <alyssaallums@Alyssas-MBP-2.agd.com>
2020-03-06 14:48:26 -0800

    q1

--- CSE163_Project.py
+++ CSE163_Project.py
@@ -1,14 +1,123 @@
 import pandas as pd
+import re
+from collections import Counter
+import math
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-def question1():
-    
+
+#majority in 2 of the 3 branches
+def calculate_party_majority(df):
+    party_majority = list()
+    for index, row in df.iterrows():
+        house = split_parties(row, "congress house")
+        senate = split_parties(row, "congress sen")
+        electoral = split_parties(row, "electoral")
+        branch_party = list()
+        branch_party.append(process_branch(house))
+        branch_party.append(process_branch(senate))
+        branch_party.append(process_branch(electoral))
+        c = Counter(branch_party)
+        #max will be one if there is a tie
+        if max(c.values()) == 1:
+            party_majority.append("Tie")
+        else:
+            party = max(c, key=c.get)
+            if is_nan(party):
+                party_majority.append(party)
+            elif party == "D":
+                party_majority.append("Democrat")
+            elif party == "R":
+                party_majority.append("Republican")
+            else:
+                party_majority.append("Other")
+    df["majority"] = party_majority
+    return df
+
+def is_nan(x):
+    return isinstance(x, float) and math.isnan(x)
+
+def split_parties(row, column):
+    if is_nan(row[column]):
+        return row[column]
+    else:
+        return row[column].split(",")
+
+def process_branch(branch):
+    if is_nan(branch):
+        return branch
+    party_count = dict()
+    for i in branch:
+        party = re.findall("[a-zA-Z]+", i)[0]
+        count = re.findall("\d+", i)
+        if len(count) == 0:
+            count = 0
+        else:
+            count = count[0]
+        if party in party_count:
+            party_count[party] += count
+        else:
+            party_count[party] = count
+    set_of_parties = get_parties_set(party_count)
+    if len(set_of_parties) == 1:
+        party = set_of_parties.pop().upper()
+        if party == "D" or party == "R":
+            return party
+        else:
+            return "O"      #Third party (other) 
+    else:
+        return "T"          #Tie between multiple parties
+
+def get_parties_set(count_dict):
+    max_count = max(count_dict.values())
+    set_of_parties = set()
+    for key, value in count_dict.items():
+        if value == max_count:
+            set_of_parties.add(key)
+    return set_of_parties
+
 
 def read_files(file):
     return pd.read_csv(file)
 
+def create_poli_gdp_df(political_party, gdp_total):
+    #filter into states that are democrat:
+    #find average of each column
+    #make new df
+    result = pd.DataFrame()
+    years = list(gdp_total.loc[:, (gdp_total.columns != "GeoFips") & (gdp_total.columns != "GeoName")].columns)
+    for year in years:
+        df1_filtered = political_party[political_party["year"] == int(year)]
+        df2_filtered = gdp_total[["GeoName", year]]
+        merged = df1_filtered.merge(df2_filtered, left_on = "state",  right_on = "GeoName", how = "left")
+        merged = merged.groupby("majority")[year].mean()
+        merged_df = pd.DataFrame({"majority":merged.index, "gdp":merged.values})
+        merged_df["year"] = [year] * len(merged_df)
+        result = pd.concat([result, merged_df], axis=0)
+    return result
+    
 
+
+def plot_political_vs_gdp(df):
+    sns.relplot(data=df, x='year', y="gdp", kind="line", hue="majority")
+    plt.xticks(rotation = 90)
+    plt.ylabel("Average gdp")
+    plt.title("Average gdp by Political Party")
+    plt.savefig("political_vs_gdp.png")
+
+
+        
+   
 def main():
-    question1()
+    gdp_percent = read_files("data/GDP_percent_change.csv")
+    gdp_total = read_files("data/GDP_total.csv")
+    unemployment = read_files("data/output.csv")
+    political_party = read_files("data/states_party_strength_cleaned2.csv")
+    political_party["state"] = political_party["state"].apply(lambda x: x.title())
+    urban_percent = read_files("data/urban_percentages.csv")
+    state_pop = read_files("data/state_populations_thousands_transposed.csv")
+    political_party = calculate_party_majority(political_party)
+    plot_political_vs_gdp(create_poli_gdp_df(political_party, gdp_total))
 
 
 if __name__ == '__main__':
