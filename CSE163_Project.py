a7f71af1465a214692e2e1d8bf83f81a15033702
libiyao <50092961+libiyao@users.noreply.github.com>
2020-03-07 16:30:32 -0800

    m

--- CSE163_Project.py
+++ CSE163_Project.py
@@ -34,14 +34,17 @@ def calculate_party_majority(df):
     df["majority"] = party_majority
     return df
 
+
 def is_nan(x):
     return isinstance(x, float) and math.isnan(x)
+
 
 def split_parties(row, column):
     if is_nan(row[column]):
         return row[column]
     else:
         return row[column].split(",")
+
 
 def process_branch(branch):
     if is_nan(branch):
@@ -68,6 +71,7 @@ def process_branch(branch):
     else:
         return "T"          #Tie between multiple parties
 
+
 def get_parties_set(count_dict):
     max_count = max(count_dict.values())
     set_of_parties = set()
@@ -77,10 +81,127 @@ def get_parties_set(count_dict):
     return set_of_parties
 
 
-def question1(poli, GDP_Total):
-    demo = dict()
-    rep = dict()
-    
+def create_poli_gdp_df(political_party, gdp_total):
+    #filter into states that are democrat:
+    #find average of each column
+    #make new df
+    result = pd.DataFrame()
+    years = list(gdp_total.loc[:, (gdp_total.columns != "GeoFips") & (gdp_total.columns != "GeoName")].columns)
+    for year in years:
+        df1_filtered = political_party[political_party["year"] == int(year)]
+        df2_filtered = gdp_total[["GeoName", year]]
+        merged = df1_filtered.merge(df2_filtered, left_on = "state",  right_on = "GeoName", how = "left")
+        merged = merged.groupby("majority")[year].mean()
+        merged_df = pd.DataFrame({"majority":merged.index, "gdp":merged.values})
+        merged_df["year"] = [year] * len(merged_df)
+        result = pd.concat([result, merged_df], axis=0)
+    return result
+
+
+def plot_political_vs_gdp(df):
+    sns.relplot(data=df, x='year', y="gdp", kind="line", hue="majority")
+    plt.xticks(rotation = 90)
+    plt.ylabel("Average gdp")
+    plt.title("Average gdp by Political Party")
+    plt.savefig("political_vs_gdp.png")
+
+
+def demographics_economy_GDP(GDP, urban):
+    result = pd.DataFrame()
+    year = list()
+    for j in range(2):
+        for i in range(1997, 2019):
+            year.append(i)      
+    result['year'] = year
+    combined = GDP.loc[1:, :].merge(urban.loc[1:, ['Area Name', '2000_per', '2010_per']], left_on='GeoName', right_on='Area Name', how='left')
+    combined = combined.dropna()
+    first_decade_urban = combined[combined['2000_per'] >= urbanized_factor(combined, '2000_per')]
+    first_decade_rural = combined[combined['2000_per'] < urbanized_factor(combined, '2000_per')]
+    second_decade_urban = combined[combined['2010_per'] >= urbanized_factor(combined, '2010_per')]
+    second_decade_rural = combined[combined['2010_per'] < urbanized_factor(combined, '2010_per')]
+    cate = list()
+    for i in range(1997, 2019):
+        cate.append('urban')
+    for i in range(1997, 2019):
+        cate.append('rural')
+    result['category'] = cate
+    result['GDP'] = make_columns(first_decade_urban, second_decade_urban) + make_columns(first_decade_rural, second_decade_rural)
+    sns.catplot(x='year', y='GDP', data=result, hue='category', kind='bar')
+    plt.xticks(rotation = 45)
+    plt.ylabel('Avearge GDP each year in rural/urban states(USD)')
+    plt.title('Demographics vs GDP')
+    plt.savefig('demographics_vs_economy_GDP.png')
+
+
+def demographics_economy_unemployment(unemploy, urban):
+    result = pd.DataFrame()
+    combined = unemploy.merge(urban.loc[1:, ['Area Name', '1990_per', '2000_per', '2010_per']], left_on='State', right_on='Area Name', how='left')
+    combined = combined.dropna()
+    rate_col = list()
+    category = list()
+    year = list()
+    cate = ['rural', 'urban']
+    for i in range(2):
+        for j in range(1990, 2017):
+            year.append(j)
+            category.append(cate[i])
+    decade = find_decade(combined, 1990, 2020)
+    for i in range(6):
+        res = decade[i].groupby(['Year', 'State'])['Rate'].mean().to_frame(name ='Rate').reset_index().groupby('Year')['Rate'].mean().tolist()
+        rate_col += res
+    result['Year'] = year
+    result['Category'] = category
+    result['Rate'] = rate_col
+    sns.catplot(x='Year', y='Rate', data=result, hue='Category', kind='bar')
+    plt.xticks(rotation = 45)
+    plt.ylabel('Average unemployment rate each year in rural/urban states')
+    plt.title('Demographics vs Unemployment rate')
+    plt.savefig('demographics_vs_economy_Unemployment.png')
+
+
+def find_decade(df, year1, year2):
+    res = list()
+    for i in range(year1, year2, 10):
+        res.append(df[(df[str(i)+'_per'] < urbanized_factor(df, str(i)+'_per')) & ((df['Year'] >= i) & (df['Year'] < i+10))])
+    for i in range(year1, year2, 10):
+        res.append(df[(df[str(i)+'_per'] >= urbanized_factor(df, str(i)+'_per')) & ((df['Year'] >= i) & (df['Year'] < i+10))])
+    return res
+
+def demographics_economy_min_wage(wage, urban):
+    result = pd.DataFrame()
+    combined = wage.merge(urban.loc[1:, ['Area Name', '1960_per', '1970_per', '1980_per', '1990_per', '2000_per', '2010_per']], left_on='State', right_on='Area Name', how='left')
+    combined['Low.Value'] = combined['Low.Value'].fillna(0)
+    wage = list()
+    category = list()
+    year = list()
+    cate = ['rural', 'urban']
+    for i in range(2):
+        for j in range(1968, 2018):
+            year.append(j)
+            category.append(cate[i])
+    decade = find_decade(combined, 1960, 2020)
+    for i in range(12):
+        res = decade[i].groupby(['Year'])['Low.Value'].mean().to_frame(name ='Low.Value').reset_index()['Low.Value'].tolist()
+        wage += res
+    result['Year'] = year
+    result['Category'] = category
+    result['Low.Value'] = wage
+    sns.catplot(x='Year', y='Low.Value', data=result, hue='Category', kind='bar')
+    plt.xticks(rotation = 90)
+    plt.ylabel('Average minimum wage each year in rural/urban states(USD)')
+    plt.title('Demographics vs Min Wage')
+    plt.savefig('demographics_vs_economy_min_wage.png')
+
+
+def make_columns(df1, df2):
+    res = list()
+    for i in range(1997, 2019):
+        res.append(df1[str(i)].mean() + df2[str(i)].mean())
+    return res
+
+
+def urbanized_factor(df, year):
+    return df.loc[:, year].mean()
 
 
 def question2(pop, GDP_Total):
@@ -127,130 +248,8 @@ def question2(pop, GDP_Total):
 
     
 def question3():
-    return None
-
-def create_poli_gdp_df(political_party, gdp_total):
-    #filter into states that are democrat:
-    #find average of each column
-    #make new df
-    result = pd.DataFrame()
-    years = list(gdp_total.loc[:, (gdp_total.columns != "GeoFips") & (gdp_total.columns != "GeoName")].columns)
-    for year in years:
-        df1_filtered = political_party[political_party["year"] == int(year)]
-        df2_filtered = gdp_total[["GeoName", year]]
-        merged = df1_filtered.merge(df2_filtered, left_on = "state",  right_on = "GeoName", how = "left")
-        merged = merged.groupby("majority")[year].mean()
-        merged_df = pd.DataFrame({"majority":merged.index, "gdp":merged.values})
-        merged_df["year"] = [year] * len(merged_df)
-        result = pd.concat([result, merged_df], axis=0)
-    return result
-    
-def plot_political_vs_gdp(df):
-    sns.relplot(data=df, x='year', y="gdp", kind="line", hue="majority")
-    plt.xticks(rotation = 90)
-    plt.ylabel("Average gdp")
-    plt.title("Average gdp by Political Party")
-    plt.savefig("political_vs_gdp.png")
-
-
-def demographics_economy_GDP(GDP, urban):
-    result = pd.DataFrame()
-    year = list()
-    for j in range(2):
-        for i in range(1997, 2019):
-            year.append(i)      
-    result['year'] = year
-    combined = GDP.loc[1:, :].merge(urban.loc[1:, ['Area Name', '2000_per', '2010_per']], left_on='GeoName', right_on='Area Name', how='left')
-    combined = combined.dropna()
-    first_decade_urban = combined[combined['2000_per'] >= urbanized_factor(combined, '2000_per')]
-    first_decade_rural = combined[combined['2000_per'] < urbanized_factor(combined, '2000_per')]
-    second_decade_urban = combined[combined['2010_per'] >= urbanized_factor(combined, '2010_per')]
-    second_decade_rural = combined[combined['2010_per'] < urbanized_factor(combined, '2010_per')]
-    cate = list()
-    for i in range(1997, 2019):
-        cate.append('urban')
-    for i in range(1997, 2019):
-        cate.append('rural')
-    result['category'] = cate
-    result['GDP'] = make_columns(first_decade_urban, second_decade_urban) + make_columns(first_decade_rural, second_decade_rural)
-    sns.catplot(x='year', y='GDP', data=result, hue='category', kind='bar')
-    plt.xticks(rotation = 45)
-    plt.ylabel('Avearge GDP each year in rural/urban states(USD)')
-    plt.title('Demographics vs GDP')
-    plt.savefig('demographics_vs_economy_GDP.png')
-
-
-
-def demographics_economy_unemployment(unemploy, urban):
-    result = pd.DataFrame()
-    combined = unemploy.merge(urban.loc[1:, ['Area Name', '1990_per', '2000_per', '2010_per']], left_on='State', right_on='Area Name', how='left')
-    combined = combined.dropna()
-    rate_col = list()
-    category = list()
-    year = list()
-    cate = ['rural', 'urban']
-    for i in range(2):
-        for j in range(1990, 2017):
-            year.append(j)
-            category.append(cate[i])
-    decade = find_decade(combined, 1990, 2020)
-    for i in range(6):
-        res = decade[i].groupby(['Year', 'State'])['Rate'].mean().to_frame(name ='Rate').reset_index().groupby('Year')['Rate'].mean().tolist()
-        rate_col += res
-    result['Year'] = year
-    result['Category'] = category
-    result['Rate'] = rate_col
-    sns.catplot(x='Year', y='Rate', data=result, hue='Category', kind='bar')
-    plt.xticks(rotation = 45)
-    plt.ylabel('Average unemployment rate each year in rural/urban states')
-    plt.title('Demographics vs Unemployment rate')
-    plt.savefig('demographics_vs_economy_Unemployment.png')
-
-def find_decade(df, year1, year2):
-    res = list()
-    for i in range(year1, year2, 10):
-        res.append(df[(df[str(i)+'_per'] < urbanized_factor(df, str(i)+'_per')) & ((df['Year'] >= i) & (df['Year'] < i+10))])
-    for i in range(year1, year2, 10):
-        res.append(df[(df[str(i)+'_per'] >= urbanized_factor(df, str(i)+'_per')) & ((df['Year'] >= i) & (df['Year'] < i+10))])
-    return res
-
-def demographics_economy_min_wage(wage, urban):
-    result = pd.DataFrame()
-    combined = wage.merge(urban.loc[1:, ['Area Name', '1960_per', '1970_per', '1980_per', '1990_per', '2000_per', '2010_per']], left_on='State', right_on='Area Name', how='left')
-    combined['Low.Value'] = combined['Low.Value'].fillna(0)
-    wage = list()
-    category = list()
-    year = list()
-    cate = ['rural', 'urban']
-    for i in range(2):
-        for j in range(1968, 2018):
-            year.append(j)
-            category.append(cate[i])
-    decade = find_decade(combined, 1960, 2020)
-    for i in range(12):
-        res = decade[i].groupby(['Year'])['Low.Value'].mean().to_frame(name ='Low.Value').reset_index()['Low.Value'].tolist()
-        wage += res
-    result['Year'] = year
-    result['Category'] = category
-    result['Low.Value'] = wage
-    sns.catplot(x='Year', y='Low.Value', data=result, hue='Category', kind='bar')
-    plt.xticks(rotation = 90)
-    plt.ylabel('Average minimum wage each year in rural/urban states(USD)')
-    plt.title('Demographics vs Min Wage')
-    plt.savefig('demographics_vs_economy_min_wage.png')
-
     
 
-
-def make_columns(df1, df2):
-    res = list()
-    for i in range(1997, 2019):
-        res.append(df1[str(i)].mean() + df2[str(i)].mean())
-    return res
-
-
-def urbanized_factor(df, year):
-    return df.loc[:, year].mean()
 
 
 def main():
